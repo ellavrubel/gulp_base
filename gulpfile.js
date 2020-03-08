099c11a0062a8b2959562b4b61916a9917efc501
ellavrubel <webella@protonmail.com>
2020-03-08 19:45:54 +0300

    concat/ uglify/ normalise/

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
-      browserSync = require('browser-sync');
+      browserSync = require('browser-sync'),
+      uglify = require('gulp-uglify'),
+      concat = require('gulp-concat');
+
 
 
 gulp.task('scss', function () {
@@ -12,6 +15,17 @@ gulp.task('scss', function () {
 
 gulp.task('html', function () {
         return gulp.src('app/*.html')
+            .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function () {
+        return gulp.src([
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+                ])
+            .pipe(concat('libs.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('app/js'))
             .pipe(browserSync.reload({stream: true}))
 });
 
