6f5a1bf4d48e6d16f8f1a6d15628028d54f7f46f
ellavrubel <webella@protonmail.com>
2020-03-09 11:00:23 +0300

    gulp project base finished

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ const gulp = require('gulp'),
       sass = require('gulp-sass'),
       browserSync = require('browser-sync'),
       uglify = require('gulp-uglify'),
-      concat = require('gulp-concat');
+      concat = require('gulp-concat'),
+      rename = require('gulp-rename');
 
 
 
 gulp.task('scss', function () {
          return gulp.src('app/scss/**/*.scss')
              .pipe(sass({outputStyle: 'compressed'}))
+             .pipe(rename({suffix: '.min'}))
              .pipe(gulp.dest('app/css'))
              .pipe(browserSync.reload({stream: true}))
 });
@@ -16,6 +18,11 @@ gulp.task('scss', function () {
 gulp.task('html', function () {
         return gulp.src('app/*.html')
             .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('script', function () {
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('js', function () {
@@ -40,8 +47,9 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function () {
         gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-        gulp.watch('app/*.html', gulp.parallel('html'))
+        gulp.watch('app/*.html', gulp.parallel('html'));
+        gulp.watch('app/js/*.js', gulp.parallel('script'));
 });
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('scss', 'js', 'browser-sync', 'watch'));
 
